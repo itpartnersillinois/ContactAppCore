68c9bedf96224f5f14fd91394578dfb2158156c4
bryanjonker <jonker@illinois.edu>
2021-07-21 12:30:37 -0500

    CodeMaid

--- ContactAppCore/Api/ContactController.cs
+++ ContactAppCore/Api/ContactController.cs
@@ -1,12 +1,9 @@
 ﻿using ContactAppCore.Data;
-using ContactAppCore.Data.Models;
 using ContactAppCore.Helpers;
 using ContactAppCore.ViewModel;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
