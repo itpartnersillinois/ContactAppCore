c283373451f7e6631d6456aae188da9afa618aef
Bryan Jonker <jonker@illinois.edu>
2022-03-30 12:19:26 -0500

    Fix accessibility issues, fix portrait / cv, add option for all usernames

--- ContactAppCore/Api/EmployeeController.cs
+++ ContactAppCore/Api/EmployeeController.cs
@@ -1,50 +1,43 @@
-﻿using ContactAppCore.Data;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ContactAppCore.Data;
 using ContactAppCore.Data.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace ContactAppCore.Api
-{
+namespace ContactAppCore.Api {
+
     [Route("api/[controller]")]
     [ApiController]
-    public class EmployeeController : ControllerBase
-    {
+    public class EmployeeController : ControllerBase {
         private IContactRepository contactRepository;
 
-        public EmployeeController(IContactRepository contactRepository)
-        {
+        public EmployeeController(IContactRepository contactRepository) {
             this.contactRepository = contactRepository;
         }
 
         [HttpGet("ByName/{netid}")]
-        public async Task<EmployeeProfile> GetEmployeeByName(string netid)
-        {
+        public async Task<EmployeeProfile> GetEmployeeByName(string netid) {
             var nameShortened = netid.Replace("@illinois.edu", "");
             var returnValue = await contactRepository.ReadAsync(c => c.EmployeeProfiles.Include(ep => ep.Jobs).ThenInclude(j => j.Office).Include(ep => ep.EmployeeLinks).Include(ep => ep.EmployeeActivities).FirstOrDefault(p => p.Title == nameShortened));
             return returnValue == null ? new EmployeeProfile() : returnValue;
         }
 
         [HttpGet("ById/{id}")]
-        public async Task<EmployeeProfile> GetEmployeeByName(int id)
-        {
+        public async Task<EmployeeProfile> GetEmployeeByName(int id) {
             var returnValue = await contactRepository.ReadAsync(c => c.EmployeeProfiles.Include(ep => ep.Jobs).ThenInclude(j => j.Office).Include(ep => ep.EmployeeLinks).Include(ep => ep.EmployeeActivities).FirstOrDefault(p => p.Id == id));
             return returnValue == null ? new EmployeeProfile() : returnValue;
         }
 
         [HttpGet("ByNameOrCreate/{netid}")]
-        public async Task<EmployeeProfile> GetEmployeeByNameOrCreate(string netid)
-        {
+        public async Task<EmployeeProfile> GetEmployeeByNameOrCreate(string netid) {
             var nameShortened = netid.Replace("@illinois.edu", "");
             var returnValue = await contactRepository.ReadAsync(c => c.EmployeeProfiles.Include(ep => ep.Jobs).ThenInclude(j => j.Office).Include(ep => ep.EmployeeLinks).Include(ep => ep.EmployeeActivities).FirstOrDefault(p => p.Title == nameShortened));
-            if (returnValue == null)
-            {
-                var employee = new EmployeeProfile
-                {
+            if (returnValue == null) {
+                var employee = new EmployeeProfile {
                     Title = User.Identity.Name.Replace("@illinois.edu", ""),
                     IsActive = true,
                     EmployeeActivities = new List<EmployeeActivity>(),
