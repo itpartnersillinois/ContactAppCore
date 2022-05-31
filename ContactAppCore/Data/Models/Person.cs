673b4c1ef5edfad8c96e0148b001210754406f27
Bryan Jonker <jonker@illinois.edu>
2022-05-31 13:24:07 -0500

    Add option to edit full biography and other information

--- ContactAppCore/Data/Models/Person.cs
+++ ContactAppCore/Data/Models/Person.cs
@@ -1,23 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace ContactAppCore.Data.Models
-{
-    public class Person : BaseDataItem
-    {
-        public Person()
-        {
+namespace ContactAppCore.Data.Models {
+
+    public class Person : BaseDataItem {
+
+        public Person() {
         }
 
-        public Person(string name, int? areaId = null, int? officeId = null)
-        {
+        public Person(string name, int? areaId = null, int? officeId = null, bool fullOfficeAdmin = false) {
             name = name.ToLowerInvariant();
             this.Title = name.EndsWith("@illinois.edu") ? name : name + "@illinois.edu";
             this.IsFullAdmin = areaId == null && officeId == null;
             this.IsActive = true;
             this.AreaId = areaId;
             this.OfficeId = officeId;
-            this.CanEditAllPeopleInUnit = false;
+            this.CanEditAllPeopleInUnit = fullOfficeAdmin;
         }
 
         public int? AreaId { get; set; }
