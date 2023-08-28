12c17cf7dc632d6cd381c0e9dd158eeb87ac2d2b
Bryan Jonker <jonker@illinois.edu>
2023-08-28 11:21:34 -0500

    Adding fields, CSS updates

---
+++ ContactAppCore/Migrations/20230825173704_AddProfileCategory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactAppCore.Migrations
+{
+    public partial class AddProfileCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "JobProfiles",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "Id",
+                keyValue: -2,
+                column: "LastUpdated",
+                value: new DateTime(2023, 8, 25, 12, 37, 3, 241, DateTimeKind.Local).AddTicks(8892));
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "LastUpdated",
+                value: new DateTime(2023, 8, 25, 12, 37, 3, 236, DateTimeKind.Local).AddTicks(1508));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "JobProfiles");
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "Id",
+                keyValue: -2,
+                column: "LastUpdated",
+                value: new DateTime(2023, 8, 25, 10, 30, 27, 364, DateTimeKind.Local).AddTicks(5825));
+
+            migrationBuilder.UpdateData(
+                table: "People",
+                keyColumn: "Id",
+                keyValue: -1,
+                column: "LastUpdated",
+                value: new DateTime(2023, 8, 25, 10, 30, 27, 358, DateTimeKind.Local).AddTicks(3653));
+        }
+    }
+}
